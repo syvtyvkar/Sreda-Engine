bb46e5a2e4efff9aed18ea2202ec4a23ff918299
Syvtyvkar <syvtyvkar@gmail.com>
2026-03-23 09:10:36 +0300

    Add argument program

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -10,10 +10,14 @@
 
 //extern Engine::EngineLayer* GenerateGame(); // Для подключения кода проектов
 
-int main()
+int main(int argc, char **argv)
 {
     Engine::Log::Init();                                                        // Инициализируем систему логирования
     Engine::Application* App = Engine::Application::CreateNewApplication();     // Создаем приложение
+    ENGINE_LOG_INFO("Read arguments program:");
+    for (int i = 0; i < argc; ++i) {
+        ENGINE_LOG_INFO("- Parameter argument: id: {} arg: {}", i, argv[i]);
+    }
     //App->PushLayer(GenerateGame());                                           // TODO: Временно отключил, мб стоит пересмотреть подход?
     App->RunApp("[OpenGL] Render");                                             // Запуск приложения
     delete App;                                                                 // Закрытие приложения
